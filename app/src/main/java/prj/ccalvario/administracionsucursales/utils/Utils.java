27364bfe10130b59a0e595ebc65d4c613d0632f3
ccalvario <ccalvariozavala@gmail.com>
2018-12-18 10:32:58 -0800

    Validar email

--- app/src/main/java/prj/ccalvario/administracionsucursales/utils/Utils.java
+++ app/src/main/java/prj/ccalvario/administracionsucursales/utils/Utils.java
@@ -1,5 +1,8 @@
 package prj.ccalvario.administracionsucursales.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Utils {
     public static boolean isRfcValid(String rfc) {
         boolean result = false;
@@ -10,7 +13,9 @@ public class Utils {
     }
 
     public static boolean isEmailValid(String email) {
-        boolean result = true;
-        return result;
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
